cd34d44eaba72f144a84360510be40993c666ade
Tyler Monaghan <33797221+tymonaghan@users.noreply.github.com>
2022-02-02 15:41:07 -0600

    apply appStyles to main pages

--- screens/MyTabs.js
+++ screens/MyTabs.js
@@ -1,11 +1,11 @@
-import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import {StyleSheet, Text, TouchableOpacity, View, Image} from 'react-native';
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {Icon} from 'react-native-elements';
 import {useNavigation} from '@react-navigation/native';
 import {auth} from '../firebase';
-import colors from '../colorConstants';
+import {colors, appStyles} from '../colorConstants';
 
 const Tab = createBottomTabNavigator();
 
@@ -29,17 +29,30 @@ const HomeScreen = () => {
   };
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.h1}>Hello, {auth.currentUser.email}</Text>
-      <Text style={styles.h2}>Welcome back to Posele!</Text>
-      <TouchableOpacity onPress={handlePlay} style={styles.primaryButton}>
-        <Text style={styles.primaryButtonText}>Play</Text>
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.secondaryButton}>
-        <Text style={styles.secondaryButtonText} onPress={handleLogout}>
-          Logout
-        </Text>
-      </TouchableOpacity>
+    <View style={appStyles.mainView}>
+      <View style={styles.container}>
+        <Text style={appStyles.heading1}>Hello, {auth.currentUser.email}</Text>
+        <Text style={appStyles.heading2}>Welcome back to Posele!</Text>
+      </View>
+      <View style={[styles.container]}>
+        <Image
+          source={require('../assets/posele-logo.png')}
+          style={[appStyles.image, styles.image]}
+        />
+      </View>
+      <View style={styles.container}>
+        <TouchableOpacity
+          onPress={handlePlay}
+          style={[appStyles.primaryButton, styles.primaryButton, appStyles.highlight]}
+        >
+          <Text style={appStyles.primaryButtonText}>Play</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={[appStyles.secondaryButton, styles.secondaryButton]}>
+          <Text style={appStyles.secondaryButtonText} onPress={handleLogout}>
+            Logout
+          </Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
@@ -94,43 +107,20 @@ export default MyTabs;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    width: '90%',
     justifyContent: 'center',
     alignItems: 'center',
   },
-  h1: {
-    fontSize: 20,
-  },
-  h2: {
-    fontSize: 20,
-  },
   primaryButton: {
-    backgroundColor: colors.primary,
     width: '60%',
-    padding: 10,
-    borderWidth: 2,
-    borderColor: colors.accent,
-    borderRadius: 10,
-    alignItems: 'center',
     marginTop: 20,
   },
   secondaryButton: {
-    backgroundColor: 'white',
-    borderColor: colors.secondary,
-    borderWidth: 3,
     width: '60%',
-    padding: 5,
-    borderRadius: 10,
-    alignItems: 'center',
     marginTop: 20,
   },
-  primaryButtonText: {
-    color: 'white',
-    fontSize: 16,
-    fontWeight: 'bold',
-  },
-  secondaryButtonText: {
-    color: colors.secondary,
-    fontSize: 16,
-    fontWeight: 'bold',
+  image: {
+    width: '35%',
+    height: '35%',
   },
 });
