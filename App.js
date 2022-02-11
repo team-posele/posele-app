7f2136350cdc9e5fa21a447aab8accf5dfd2c565
Tyler Monaghan <33797221+tymonaghan@users.noreply.github.com>
2022-02-11 17:42:28 -0600

    remove share screen

--- App.js
+++ App.js
@@ -48,7 +48,6 @@ export default function App() {
         <Stack.Screen name="CapturePose" options={{headerShown: false}} component={CapturePose} />
         <Stack.Screen name="Results" options={{headerShown: false}} component={SinglePoseResults} />
         <Stack.Screen name="NoPose" options={{headerShown: false}} component={NoPose} />
-        <Stack.Screen name="Share" options={{headerShown: false}} component={Share} />
       </Stack.Navigator>
     </NavigationContainer>
   );
